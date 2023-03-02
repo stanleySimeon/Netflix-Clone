18de0624f5da9c525687744845eaf3cfb9d02d08
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-03-02 01:33:43 -0500

    Update api

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,28 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMovie } from '../redux/reducers/moviesReducer';
 
 export default function Stream() {
   const dispatch = useDispatch();
   const movies = useSelector((state) => state.movies[0]) || [];
+  const [query, setQuery] = useState('blade runner');
 
   useEffect(() => {
-    dispatch(getMovie());
-  }, [dispatch]);
+    dispatch(getMovie(query));
+  }, [query, dispatch]);
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+
+    if (e.target.value === '') {
+      setQuery('batman');
+    } else {
+      setQuery(e.target.value);
+    }
+  };
 
   return (
     <div>
       <h1>Stream</h1>
-      {movies.map((movie) => (
-        <div key={movie.id} id={movie.id}>
-          <img src={movie.poster} alt={movie.Title} />
-          <h3>{movie.title}</h3>
-          <p>{movie.year}</p>
-          <p>{movie.rating}</p>
-          <p>{movie.plot}</p>
-          <button type="button">Play</button>
-        </div>
-      ))}
+      <input
+        type="text"
+        id="search"
+        placeholder="Search for a movie"
+      />
+      <button type="button" onClick={handleSearch}>Search</button>
+      {
+        movies.map((movie) => (
+          <div key={movie.id} onChange={handleSearch}>
+            <h2>{movie.title}</h2>
+            <img src={movie.image} alt={movie.title} />
+            <p>{movie.summary}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
