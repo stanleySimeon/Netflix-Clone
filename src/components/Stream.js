bdf4a67d62669f6a3243ef929f2b0b9069962521
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-03-04 20:30:55 -0500

    Update stream

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -16,22 +16,19 @@ export default function Stream() {
       <Navigation />
       <div className="row">
         {movies.map((movie) => (
-          <div className="col-md-4" key={movie.id}>
-            <div className="card mb-4 shadow-sm">
-              <img src={movie.image} alt={movie.title} />
+          <div className="col-4" key={movie.id}>
+            <div className="card">
+              <img
+                src={movie.image}
+                className="card-img-top"
+                alt={movie.title}
+              />
               <div className="card-body">
-                <p className="card-text">{movie.title}</p>
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="btn-group">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      View
-                    </button>
-                  </div>
-                  <small className="text-muted">{movie.year}</small>
-                </div>
+                <h5 className="card-title">{movie.title}</h5>
+                <p className="card-text">{movie.description}</p>
+                <a href="/" className="btn btn-primary">
+                  Go somewhere
+                </a>
               </div>
             </div>
           </div>
