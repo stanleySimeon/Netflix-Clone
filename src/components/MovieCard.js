16da23f5fd4b36e8cdcfc76155483e3a926de198
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-03-16 23:38:25 -0400

    Update featured Movie and movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -12,13 +12,8 @@ function MovieCard({ movie, saveFeaturedMovie }) {
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-4 mb-8" onClick={handleClick}>
       <div className="relative">
         <img src={movie.image} alt={movie.title} className="rounded" />
-        <div className="overlay absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-75 transition-opacity duration-500">
-          <button type="button" className="bg-red-600 py-2 px-4 rounded-md font-bold">
-            Play
-          </button>
-        </div>
+        <div className="overlay absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-75 transition-opacity duration-500" />
       </div>
-      <p className="font-bold mt-2">{movie.title}</p>
     </div>
   );
 }
