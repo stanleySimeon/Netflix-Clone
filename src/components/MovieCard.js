477f359202abbb8bca2ece8057a32818b512474b
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-03-20 20:51:17 -0400

    Update stream and movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import PropTypes from 'prop-types';
 
 function MovieCard({ movie, saveFeaturedMovie }) {
@@ -11,8 +13,57 @@ function MovieCard({ movie, saveFeaturedMovie }) {
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-4 mb-8" onClick={handleClick}>
       <div className="relative">
-        <img src={movie.image} alt={movie.title} className="rounded" />
-        <div className="overlay absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-75 transition-opacity duration-500" />
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlay
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className="w-full h-full flex mt-8 lg:mt-12 z-10"
+          containerClass="container-with-dots"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 3,
+              partialVisibilityGutter: 40,
+            },
+            mobile: {
+              breakpoint: {
+                max: 639,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464,
+              },
+              items: 2,
+              partialVisibilityGutter: 30,
+            },
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          <img src={movie.image} alt={movie.title} className="rounded" />
+          <div className="overlay absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-75 transition-opacity duration-500" />
+        </Carousel>
       </div>
     </div>
   );
